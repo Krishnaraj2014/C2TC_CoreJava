5ad32d00e82fd8a619cf7ad0569a45e008f25d60
krish <krish@KRISHNARAJ>
2023-07-30 21:43:30 +0530

    Day 1 programs

---
+++ main/src/Day1/IdentifersProgram.java
@@ -0,0 +1,13 @@
+package Day1;
+
+public class IdentifersProgram {
+
+	public static void main(String[] args) {
+		int age = 20;
+		System.out.println("My age is : "+ age);
+		String studentName="Vetri";
+		System.out.println("My name is : "+ studentName);
+
+	}
+
+}
